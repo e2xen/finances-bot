8ed68e7f500997fa44d206bf86a17f839c77edc7
e2xen <max.ks1230@gmail.com>
2022-11-01 03:06:33 +0300

    [GOHW-5]: add logging, tracing, and metrics

---
+++ TRACING.md
@@ -0,0 +1,6 @@
+# Трейсинг
+
+Примеры получившихся трейсов:
+![](https://i.imgur.com/Pbmia3Y.png)
+![](https://i.imgur.com/FNhPg5U.png)
+![](https://i.imgur.com/ANVWdqb.png)
--- internal/model/messages/incoming_msg.go
+++ internal/model/messages/incoming_msg.go
@@ -1,6 +1,15 @@
 package messages
 
-import "context"
+import (
+	"context"
+	"strconv"
+	"time"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
 
 type messageSender interface {
 	SendMessage(text string, userID int64) error
@@ -27,7 +36,35 @@ type Message struct {
 	UserID int64
 }
 
+var HistogramResponseTime = promauto.NewHistogramVec(
+	prometheus.HistogramOpts{
+		Namespace: "route256",
+		Subsystem: "telegram",
+		Name:      "histogram_response_time_seconds",
+		Buckets:   []float64{0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 2},
+	},
+	[]string{"status"},
+)
+
 func (s *Service) HandleIncomingMessage(ctx context.Context, msg Message) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "handleMessage")
+	defer span.Finish()
+
+	start := time.Now()
+	err := s.handle(ctx, msg)
+	elapsed := time.Since(start)
+
+	HistogramResponseTime.
+		WithLabelValues(strconv.FormatBool(err != nil)).
+		Observe(elapsed.Seconds())
+
+	if err != nil {
+		ext.Error.Set(span, true)
+	}
+	return err
+}
+
+func (s *Service) handle(ctx context.Context, msg Message) error {
 	resp, err := s.handler.HandleMessage(ctx, msg.Text, msg.UserID)
 	if err != nil {
 		_ = s.tgClient.SendMessage("Sorry, something wrong happened...\n"+resp, msg.UserID)
